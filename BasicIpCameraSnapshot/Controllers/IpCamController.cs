5e3d28c3ecda494c4f41839bc6d6783a711a46ec
21h2 <21h2@DESKTOP-PDHE404>
2022-07-29 15:19:10 -0400

    Add project files.

---
+++ BasicIpCameraSnapshot/Controllers/IpCamController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkiaSharp;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication4.Model;
+
+namespace WebApplication4.Controllers
+{
+    public class IpCamController : Controller
+    {
+        static readonly ConcurrentDictionary<string, CachedImage> img = new();
+
+        class CachedImage
+        {
+            public Stopwatch Stopwatch { get; } = Stopwatch.StartNew();
+            public byte[] Image { get; init; }
+        }
+
+        public IActionResult Index([FromServices] ISettings settings)
+        {
+            return View(settings.Cameras.Keys.ToList());
+        }
+
+        public async Task<IActionResult> GetImage([FromServices] ISettings settings, [FromServices] IHttpClientFactory clientFactory, string id, bool thumb = false)
+        {
+            if (img.TryGetValue(id, out CachedImage cached) && cached.Stopwatch.ElapsedMilliseconds < settings.CacheTime)
+                return ProperSize(cached.Image, thumb);
+
+            if (settings.Cameras.TryGetValue(id, out (string credential, string link) info))
+            {
+                string authString = Convert.ToBase64String(Encoding.UTF8.GetBytes(info.credential));
+
+                using var client = clientFactory.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authString);
+
+                using var request = new HttpRequestMessage(HttpMethod.Get, info.link);
+                using var response = await client.SendAsync(request);
+
+                cached = new CachedImage()
+                {
+                    Image = await response.Content.ReadAsByteArrayAsync()
+                };
+
+                img[id] = cached;
+                
+                return ProperSize(cached.Image, thumb);
+            }
+
+            return NotFound(); 
+        }
+
+        private FileContentResult ProperSize(byte[] rawImage, bool thumb)
+        {
+            byte[] resizedRawImage = null;
+
+            if (thumb)
+            {
+                using var original = SKBitmap.Decode(rawImage);
+                var resizeInfo = new SKImageInfo(original.Width / 4, original.Height / 4);
+
+                using var resized = original.Resize(resizeInfo, SKFilterQuality.High);
+                using var rawData = resized.Encode(SKEncodedImageFormat.Jpeg, 90);
+
+                resizedRawImage = rawData.ToArray();
+            }
+
+            return File(resizedRawImage ?? rawImage, "image/jpg");
+        }
+    }
+}
---
+++ BasicIpCameraSnapshot/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using WebApplication4.Model;
+
+namespace WebApplication4
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddHttpClient();
+            
+            services.AddSingleton<ISettings, Settings>(x => new Settings()
+            {
+                CacheTime = Configuration.GetSection("Camera").GetValue<int>("CacheTime"),
+                Cameras = Configuration.GetSection("Camera").GetSection("Cameras")
+                                       .GetChildren()
+                                       .ToDictionary(k => k.GetValue<string>("Id"), 
+                                                     v => (v.GetValue<string>("Credential"), v.GetValue<string>("Link")))
+            }
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
